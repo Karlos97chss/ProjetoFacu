7bad829f5073341f7e8e095441a4b153da85d375
Karlos97chss <sanchescarlosh@gmail.com>
2024-03-07 21:49:14 -0300

    aula4

---
+++ aulas/aula04/index.js
@@ -0,0 +1,50 @@
+const express = require('express');
+
+const app = express();
+
+// middleware integrado
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
+// middleware de app
+app.use(function (req, res, next) {
+    console.log("Data/Hora", new Date());
+    console.log('Url:', req.url);
+    console.log('Metodo: ', req.method);
+    console.log('Parametros: ', req.params);
+    console.log('Corpo: ', req.body);
+    console.log('Cabecalho: ', req.headers);
+    next();
+});
+
+// middleware de rota
+app.get('/', function(req, res){
+    res.status(200).end();
+    //res.send('Metodo GET');
+    //res.json({codigo: 200, mensagem: 'De boas'})
+});
+
+app.post('/', function(req, res){
+    console.log(req.body);
+    res.status(204).end();
+});
+
+app.put( '/' , function( req , res ){
+    res.status(204).end();
+});
+
+app.delete('/', (req, res) =>{
+    throw new Exception('Erro');
+});
+
+// middleware de tratamento de erro
+app.use(function(error, req, res, next){
+    res.status(400).send({message: "Deu ruim"});
+});
+
+app.listen(3000, function(){
+    console.log('API está ON!');
+});
+
+
+module.exports = app;
